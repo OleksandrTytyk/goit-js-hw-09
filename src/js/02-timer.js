7e323e824cfcea69c7a9479d485d83eadb3a4128
Oleksandr Tytyk <aleks.tit3@gmail.com>
2023-02-05 14:39:14 +0200

    add 2 task

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,6 +1,8 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import Notiflix from 'notiflix';
+require('flatpickr/dist/themes/dark.css');
+
 let timerId = null;
 
 const refs = {
@@ -10,65 +12,55 @@ const refs = {
   hoursEl: document.querySelector('[data-hours]'),
   minutesEl: document.querySelector('[data-minutes]'),
   secondsEl: document.querySelector('[data-seconds]'),
+  timer: document.querySelector('.timer'),
 };
 refs.startBtnEl.disabled = true;
-// const timer = document.querySelector('.timer');
 
 const options = {
+  altInput: true,
+  altFormat: 'F j, Y',
+  dateFormat: 'Y-m-d',
+
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
     if (selectedDates[0] < new Date()) {
-      alert('Choose normal date');
+      Notiflix.Notify.failure('Return to the past? Bad idea!');
+
     } else {
-        refs.startBtnEl.disabled = false;
-        refs.startBtnEl.addEventListener('click', () => {
-          timerId = setInterval(() => {
-            const selectedTimeEl = selectedDates[0].getTime() - Date.now();
-                const timer = convertMs(selectedTimeEl)
-              console.log(timer);
-          }, 1000);
-        });
+                      Notiflix.Notify.success('Push the start button!');
 
-        // refs.startBtnEl.addEventListener('click', () => {
-        //     const selectedTimeEl = selectedDates[0].getTime() - Date.now();
-        //     const timer = convertMs(selectedTimeEl)
-        //     console.log("refs.startBtnEl.addEventListener ~ timer", timer);
-        // })
+      refs.startBtnEl.disabled = false;
+      refs.startBtnEl.addEventListener('click', () => {
+        timerId = setInterval(() => {
+          const selectedTimeEl = selectedDates[0].getTime() - Date.now();
+          convertMs(selectedTimeEl);
+
+        }, 1000);
+      });
     }
-    },
-  
+  },
 };
-
-// function startTimer(timer) {
-    
-// }
-
-function updateClockFace({ days, hours, minutes, seconds }) {
-  refs.daysEl.textContent = days;
-  refs.hoursEl.textContent = hours;
-  refs.minutesEl.textContent = minutes;
-  refs.secondsE.textContent = seconds;
-}
 flatpickr(refs.inputEl, options);
 
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
-  // Remaining days
   const days = Math.floor(ms / day);
-  // Remaining hours
   const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
   const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  refs.timer.textContent = `${days} Day(s) ${addLeadingZero(
+    hours
+  )} Hour(s) ${addLeadingZero(minutes)} Minute(s) ${addLeadingZero(
+    seconds
+  )} Second(s)`;
 
   return { days, hours, minutes, seconds };
 }
@@ -76,22 +68,3 @@ function convertMs(ms) {
 function addLeadingZero(value) {
   return value.toString().padStart(2, '0');
 }
-
-// refs.startBtnEl.addEventListener('click', () => {
-//   let timer = setInterval(() => {
-//     let count = new Date(input.value) - new Date();
-//     console.log("timer ~ count", count);
-//     startBtn.disabled = true;
-//     if (count >= 0) {
-//       let timerValue = convertMs(count);
-//       days.textContent = addLeadingZero(timerValue.days);
-//       hours.textContent = addLeadingZero(timerValue.hours);
-//       minutes.textContent = addLeadingZero(timerValue.minutes);
-//       seconds.textContent = addLeadingZero(timerValue.seconds);
-//       if (count <= 10000) timerHtml.style.color = 'red';
-//     } else {
-//       Notiflix.Notify.success('The countdown is complete');
-//       clearInterval(timer);
-//     }
-//   }, 1000);
-// });
